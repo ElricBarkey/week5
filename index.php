8de1a701f038c63543593e6ae6eaeae88a82f1da
Elric Barkey <ebarkey2@mail.greenriver.edu>
2020-05-04 17:49:12 -0400

    updated files

--- index.php
+++ index.php
@@ -14,11 +14,32 @@ require_once ("vendor/autoload.php");
 $f3 = Base::instance();
 
 //default route
-$f3->route('GET /', function()
+$f3->route('GET /', function($f3)
 {
-    echo "<h1>Week 5</h1>";
-    //$view = new Template();
-    //echo $view->render('views/pet-home.html');
+    //echo "<h1>Week 5</h1>";
+
+    //create variables in the hive
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with templates');
+    $f3->set('temp', 67);
+    $f3->set('color', 'purple');
+    $f3->set('radius', 10);
+
+    //define array of fruits
+    $f3->set('fruits', array('apple', 'orange', 'pear'));
+    $f3->set('bookmarks', array('www.google.com', 'www.greenriver.edu', 'www.'));
+
+    $f3->set('desserts', array("chocolate"=>"Chocolate Mousse", "vanilla"=>"Vanilla Custard", "strawberry"=>"Strawberry 
+    Shortcake"));
+
+    //conditional content
+    $f3->set('preferredCustomer', true);
+    $f3->set('lastLogin', strtotime('-1 week'));
+
+    //Display the template
+    $view = new Template();
+    echo $view->render('views/info.html');
 });
 
 
